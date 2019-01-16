508973da85b1e1f277cd468bb120af0ff6f2ba1c
Daniel Egbers <daniel@egbers.site>
2019-01-16 23:22:08 +0100

    added "Update Binaries.ps1"

---
+++ AndroidSDK.PlatformTools/Update Binaries.ps1
@@ -0,0 +1,38 @@
+param (
+  [string]$Version
+)
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$ProgressPreference = 'SilentlyContinue'
+
+if ([string]::IsNullOrWhitespace($Version)) {  
+  $Version = Read-Host "Version" 
+}
+$url = "https://dl.google.com/android/repository/platform-tools_r$version-windows.zip"
+
+$tempPath = Join-Path (Get-Location) "tmp"
+if (Test-Path $tempPath)
+{
+  Remove-Item $tempPath -Recurse
+}
+New-Item -ItemType Directory $tempPath | Out-Null
+
+Write-Host "Downloading $url"
+Invoke-WebRequest $url -OutFile "$tempPath\platform-tools.zip"
+
+Write-Host "Extracting $tempPath\platform-tools.zip"
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$tempPath\platform-tools.zip", $tempPath)
+
+$dataPath = Join-Path (Get-Location) "Binaries"
+if (Test-Path $dataPath)
+{
+	Remove-Item $dataPath -Recurse
+}
+New-Item -ItemType Directory $dataPath | Out-Null
+
+Write-Host "Copying $tempPath\platform-tools to $dataPath"
+Copy-Item "$tempPath\platform-tools\*" "$dataPath\" -Recurse -Include @("adb.exe", "fastboot.exe", "AdbWinApi.dll", "AdbWinUsbApi.dll")
+
+(& "$dataPath\adb.exe" --version)[0]
+(& "$dataPath\fastboot.exe" --version)[0]
